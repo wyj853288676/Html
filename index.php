f09065f1496eac66ca0556c9ad584c5f720af16b
wyj <853288676@qq.com>
2019-01-05 13:42:35 +0800

    111

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <html>
 <head>
     <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <title>无聊写写</title>
     <link rel="stylesheet" href="myCss.css">
     <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
