fc399bda42cf4c737e90593d53ce5eb10222ed94
wyj <853288676@qq.com>
2019-01-06 22:22:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,9 @@
         <div class="navbar-collapse collapse" role="navigation">
             <ul class="nav nav-tabs ">
                 <?php foreach($list1 as $value):?>
-                    <li class=""><a href="#<?=str_replace(["$path\\",'.html'],"",$value);?>" data-toggle="tab" ><?=str_replace(["$path\\",'.html'],"",$value);?></a></li>
+                    <li class=""><a href="#<?=str_replace(["$path\\",'.html'],"",$value);?>"  data-toggle='tab'><?=str_replace(["$path\\",'.html'],"",$value);?></a></li>
                 <?php endforeach;?>
+                <li><a href="/table/index.html" target="_blank" >table</a></li>
             </ul>
         </div>
         <div class='tab-content'>
@@ -111,8 +112,6 @@
         .collapse {
             display: none;
         }
-
-
     }
    
 </style>
