972fce7e3cdff4a5ebd1d049cd780e48371ca38c
wyj <853288676@qq.com>
2019-01-06 21:45:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,24 +10,34 @@
     <link rel="stylesheet" href="myCss.css">
     <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
-	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
 </head>
 <body style='font-size:20px'>
     <div class='row'>
-    <ul class="nav nav-tabs">
-        <?php foreach($list1 as $value):?>
-            <li class=""><a href="#<?=str_replace(["$path\\",'.html'],"",$value);?>" data-toggle="tab" ><?=str_replace(["$path\\",'.html'],"",$value);?></a></li>
-        <?php endforeach;?>
-    </ul>
-    <div class='tab-content'>
-        <?php foreach($list1 as $value):?>
-            <div class='tab-pane fade in ' id='<?=str_replace(["$path\\",'.html'],"",$value);?>'>
-                <?php include(str_replace("$path\\","",$value));?>
-            </div>
-        <?php endforeach;?>
+        <div class="navbar-header">
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand hidden-sm" href="#" disabled>列表</a>
+        </div>
+        <div class="navbar-collapse collapse" role="navigation">
+            <ul class="nav nav-tabs ">
+                <?php foreach($list1 as $value):?>
+                    <li class=""><a href="#<?=str_replace(["$path\\",'.html'],"",$value);?>" data-toggle="tab" ><?=str_replace(["$path\\",'.html'],"",$value);?></a></li>
+                <?php endforeach;?>
+            </ul>
+        </div>
+        <div class='tab-content'>
+            <?php foreach($list1 as $value):?>
+                <div class='tab-pane fade in ' id='<?=str_replace(["$path\\",'.html'],"",$value);?>'>
+                    <?php include(str_replace("$path\\","",$value));?>
+                </div>
+            <?php endforeach;?>
+        </div>
     </div>
-    </div>
-   
 </body>
 <style>
     body{
@@ -43,6 +53,68 @@
         box-shadow: 0px 0px 10px #11111179;
         
     }
+    .nav.nav-tabs{
+        background-color:black;
+        border-bottom: 1px #7b7474 solid;
+    }
+   .navbar-header a{
+        color:white;
+    }
+    .nav.nav-tabs a{
+        color:#7b7474;
+        font-weight:500;
+    }
+    .nav.nav-tabs li{
+        border-radius:0;
+    }
+    .nav.nav-tabs li.active a{
+        color:white;
+        background-color:black;
+        border-radius:0;
+        border-width: 0px 0px 2px 0px ;
+        border-style:solid;
+        border-color:red !important;
+    }
+    .nav.nav-tabs li a:hover{
+        border-width: 0px 0px 2px 0px ;
+        border-style:solid;
+        border-color:red !important;
+    }
+    .nav.nav-tabs a:hover{
+        color:white;
+        background-color:black;
+        border:0px solid;
+    }
+    .navbar-header button{
+        border: 1px #7b7474 solid;
+    }
+    .navbar-toggle .icon-bar{
+        background-color: white;
+    }
+    .navbar-collapse{
+            padding-left:0px;
+            padding-right:0px;
+            width:100%;
+            position:fixed;
+            z-index:1;
+    }
+    @media screen and (max-width:900px){
+        .nav.nav-tabs li{
+            float:none !important;
+        }
+        .navbar-header{
+            background-color:black;
+        }
+        .navbar-header button{
+            display:block;
+        }
+        .collapse {
+            display: none;
+        }
+
+
+    }
+   
 </style>
 <script>
     
