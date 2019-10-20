ee3140adb6ab04a87c1a962266ff1aa6500ffd2c
wyj853288676 <853288676@qq.com>
2019-10-20 10:17:17 +0800

    plugins

--- share/common/swiperAxis.js
+++ share/common/swiperAxis.js
@@ -1,15 +1,18 @@
 ;(function($){
     $.fn.swiperAxis=function(inOptions){
         let options={
-
+            alignVice:true,
+            moveSpeed:function(){return _this.find('div.swiper-axis-content').length>0?_this.find('div.swiper-axis-content:eq(0)').width():_this.width();},
+            fixWidth:false,
         };
+        options=$.extend(options,inOptions);
         let _this=$(this).addClass('swiper-axis-container');
-        let hasViceTitle=false;
-        let _leftBtn=$(document.createElement('button')).addClass('swiper-left-btn swiper-btn');
-        let _rightBtn=$(document.createElement('button')).addClass('swiper-right-btn swiper-btn');
-        let _contents=_this.children('div.swiper-axis-content')
-        let _firstContent;
+        let _leftBtn=$(document.createElement('button')).addClass('swiper-left-btn swiper-btn').attr('type','button');
+        let _rightBtn=$(document.createElement('button')).addClass('swiper-right-btn swiper-btn').attr('type','button');
+        let _contents=_this.children();
+        let _object=$(document.createElement('div')).addClass('swiper-object');
         let maxX,minX;//limit
+        let correctMax,correctMin;
         let containerWidth;
         let contentWidth;
         initDom();
@@ -21,6 +24,8 @@
 
 
         function initDom(){
+            _object.append(_contents);
+            _this.append(_object);
             //light
             _contents.each(function(index){
                 let _content=$(this);
@@ -29,10 +34,9 @@
                 }   
                 _content.children("div.swiper-axis-title").after('<i class="swiper-axis-light"></i>');
                 if(_content.children('div.swiper-axis-vice-title').length>0){
-                    hasViceTitle=true;
                 }
             });
-            if(hasViceTitle){
+            if(options['alignVice']){
                 _leftBtn.addClass('align-vice');
                 _rightBtn.addClass('align-vice');
             }
@@ -41,17 +45,19 @@
 
         function align(){
             contentWidth=getContentWidth();
+            if(options['fixWidth']&&getContentWidth()<_this.width()){
+                _this.width(getContentWidth());
+            }
             containerWidth=_this.width();
             if(contentWidth<=containerWidth){
-                maxX=(containerWidth-contentWidth)/2+250;
-                minX=(containerWidth-contentWidth)/2+250;
-                move((containerWidth-contentWidth)/2);
+                correctMax=correctMin=(containerWidth-contentWidth)/2;
             }else{
-                maxX=250;
-                minX=-250-40+(containerWidth-contentWidth);
-                move(-40+(containerWidth-contentWidth));
+                correctMax=40;
+                correctMin=-40+(containerWidth-contentWidth)
             }
-
+            maxX=correctMax+250;
+            minX=correctMin-250;
+            move(correctMin);
         }
 
         function bindListen(){
@@ -72,13 +78,13 @@
                     x=getX();
                     distance=x;
                 }
-                if(distance>40){
+                if(distance>correctMax){
                     func=function(){
-                        move(40,{useRaf:true,frame:30});
+                        move(correctMax,{useRaf:true,frame:30});
                     }
-                }else if(distance<(-40+containerWidth-contentWidth)){
+                }else if(distance<correctMin){
                     func=function(){
-                        move(-40+containerWidth-contentWidth,{useRaf:true,frame:30});
+                        move(correctMin,{useRaf:true,frame:30});
                     }
                 }
                 if(Math.abs(e.pageX-recordIndex)>100){
@@ -102,13 +108,21 @@
             _this.on('mousedown',function(e){
                 mousedown=true;
                 recordIndex=index=e.pageX;
-                _firstContent[0].rafCount= _firstContent[0].rafCount!=undefined?( _firstContent[0].rafCount+1):0;
+                _object[0].rafCount= _object[0].rafCount!=undefined?( _object[0].rafCount+1):0;
             });
+            $(document).on('mousemove',function(e){
+                moveCount++;
+                if(moveCount>limit){
+                    moveCount=0;
+                    recordIndex=e.pageX;
+                }
+            })
             _this.on('mousemove',function(e){
-                e.preventDefault();
                 if(!mousedown||index==undefined){
                     return true;
                 }
+                e.stopPropagation();
+                e.preventDefault();
                 moveCount++;
                 if(moveCount>limit){
                     moveCount=0;
@@ -117,30 +131,42 @@
                 move(e.pageX-index+getX());
                 index=e.pageX;
             });
+
+            $(window).on('resize',function(){
+                align();
+            })
         }
 
 
         function listenBtn(){
             let distance;
+            let correctDistance;
+            let isMoving=false;
             _rightBtn.on('click',function(){
-                distance=getX()-_firstContent.width();
-                btnClickHandler()
+                distance=getX()-options['moveSpeed']();
+                // _object[0].rafCount=_object[0].rafCount==undefined?0:_object[0].rafCount++;
+                btnClickHandler();
             })
             _leftBtn.on('click',function(){
-                distance=getX()+_firstContent.width();
+                distance=getX()+options['moveSpeed']();
+                // _object[0].rafCount=_object[0].rafCount==undefined?0:_object[0].rafCount++;
                 btnClickHandler();
             })
 
             let btnClickHandler=function(){
-                let func=function(){};
-                console.log(distance,-40+containerWidth-contentWidth);
-                if(distance>40){
+                if(isMoving){
+                    return true;
+                }
+                isMoving=true;
+                let func=function(){isMoving=false;};
+                if(distance>correctMax||distance<correctMin){
+                    correctDistance=distance>40?correctMax:correctMin;
                     func=function(){
-                        move(40,{useRaf:true,frame:30});
-                    }
-                }else if(distance<(-40+containerWidth-contentWidth)){
-                    func=function(){
-                        move(-40+containerWidth-contentWidth,{useRaf:true,frame:30});
+                        move(correctDistance,{
+                            useRaf:true,
+                            frame:30,
+                            func:function(){isMoving=false}
+                        });
                     }
                 }
                 move(distance,{useRaf:true,func:func});
@@ -156,29 +182,24 @@
             };
             moveOptions=$.extend(moveOptions,funOptions);
             if(x>maxX){
-                distance=maxX;
                 x=maxX;
             }else if(x<minX){
-                distance=minX;
                 x=minX;
             }
             if(!moveOptions['useRaf']){
-                _firstContent.css('margin-left',x);
+                _object.css('margin-left',x);
                 moveOptions['func']();
             }else{
-                rafMove(x,[_firstContent],'margin-left',moveOptions['frame'],moveOptions['func']);
+                rafMove(x,[_object],'margin-left',moveOptions['frame'],{func:moveOptions['func']});
             }
         }
         function getX(){
-            return  parseFloat(_firstContent.css('margin-left').replace('px',''))
+            return  parseFloat(_object.css('margin-left').replace('px',''))
         }
 
         function getContentWidth(){
-            let contentWidth=0;
-            _contents.each(function(index){
-                contentWidth+=$(this).outerWidth();
-            });
-            return contentWidth;
+    
+            return _object.outerWidth();;
         }
 
     };
@@ -188,9 +209,13 @@
 
 
 //raf使得滚动效果更加平滑 frame可以手动设置
-function rafMove(y,doms,css,f,func){
-    f=arguments[3]?arguments[3]:0;
-    func=arguments[4]?arguments[4]:function(){};
+function rafMove(y,doms,css,f,inOptions){
+    let options={
+        func:function(){},
+    }
+    inOptions=arguments[4]?arguments[4]:0;
+    options=$.extend(options,inOptions);
+    let func=options['func'];
     let index=parseFloat(doms[0].css(css).replace('px',''));
     let originIndex=index;
     let frame=f==0?5:f;
@@ -202,19 +227,23 @@ function rafMove(y,doms,css,f,func){
     let count=0;
     move();
     function move(){
-         if(frame<0||(rafSignal-doms[0][0].rafCount)<0){
-            func();
-            return false;
-        }
         if(f>15){
             addNum=Math.pow(count-f,2)*(originIndex-y)/Math.pow(f,2)+(y-index);
         }
         for(let i in doms){
+            console.log();
             doms[i].css(css,(index)+'px');
         }
+        //最小1px
+        addNum=Math.abs(addNum)<1&&addNum!=0?Math.abs(addNum)/addNum:addNum;
         index+=addNum;
-        requestAnimationFrame(move);
         frame--;
         count++;
+        if(frame<0||(rafSignal-doms[0][0].rafCount)<0||Math.abs(index-originIndex)>=Math.abs(y-originIndex)){
+            func();
+            return false;
+        }
+        requestAnimationFrame(move);
+  
     }
 }
